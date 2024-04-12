881f8595301a0542fca411a59dfa82b0f3060390
Fuad-hasan001 <spotlight.neutron0@gmail.com>
2024-04-13 00:29:10 +0600

    Project Initialize and setup Environmenr

---
+++ JS/ticket.js
@@ -0,0 +1,7 @@
+console.log("from Smart Ticketing JS file");
+/* 
+    date: 13/04/2024
+    Created by : Fuad Hasan
+    Project: JS-DOM: Smart-Ticketing
+    Tools: HTML5, CSS3, JS-DOM, TailwindCss + daisyUI
+*/
